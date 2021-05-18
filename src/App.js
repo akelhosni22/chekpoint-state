31d3db3b1ae47757095bf2ebff8654eb93d8ec5a
Akel <akel.hosni@gmail.com>
2021-05-18 17:09:28 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,75 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import './App.css'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component{
+
+  constructor (props){
+
+    super(props);
+      this.state={
+      fullname: <h1>Hosni akel</h1>,
+      bio:<p>fullstack Js developer  with 2 years of experience in Javascript,react, mongo D8, etc. Received best Award and Best StudentTeam while working at Go my Code. "</p>,
+      profession:<h2>FullStack JavaScript Developer </h2>,
+      visible: false ,
+      intervall: null ,
+      timer: 0 ,
+      };
+    }
+  componentDidMount(){
+
+    this.setState({
+      intervall : setInterval(() => {
+        this.setState({timer: this.state.timer + 1 }) ;
+
+      }, 1000) ,
+    });
+
+  }
+  componentDidUpdate(){
+    console.log("componentDidUpdate()") ;
+
+  }
+  componentWillUnmount(){
+
+    clearInterval(this.state.timer);
+
+  }
+  togglevisiblity = () => {
+    this.setState({
+      visible : !this.state.visible,
+    })
+  }
+
+
+
+  render(){
+
+    return(
+      <div>
+        <div className="para">
+          <h3> click ON to get more informations</h3>
+        </div>
+          {this.state.visible && (
+            <div>
+                <div>
+                  <img src='/akel.jpg'></img>
+                </div>
+              {this.state.fullname}
+              {this.state.bio}
+              {this.state.profession}
+              <h2>{this.state.timer}</h2>
+            </div>
+          )
+          }
+
+          <br />
+          <br />
+            <button onClick={this.togglevisiblity}>
+          {this.state.visible ? "OFF" : "ON"} 
+        </button>
+      </div>
+    )
+
+  }
 }
-
-export default App;
+export default App ;
